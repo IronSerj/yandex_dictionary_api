8b218a708c0f296e85a9476b04b66dc41e6de0e6
IronSerj <user@user-VirtualBox.(none)>
2014-09-05 21:12:09 +0400

    some cleaning.

--- lib/yandex_dictionary_api/api_client.rb
+++ lib/yandex_dictionary_api/api_client.rb
@@ -14,19 +14,10 @@ module YandexDictionaryApi
     def execute(str_req_type, params_hash)
       str_request = BASE_URI + "/#{str_req_type}?"
       params_hash["key"] = @api_key
-      #str_request = self.add_parameters_to_request(str_request, params_hash)
       self.exec_request_http(str_request, params_hash)
     end
 
     protected
-
-    def add_parameters_to_request(str_request, params_hash)
-      str_res = str_request + "key=#{@api_key}"
-      params_hash.each do |(key, value)|
-        str_res += "&#{key}=#{value}"
-      end
-      str_res
-    end
 
     def exec_request_http(str_uri, params_hash)
       @http_client.post(str_uri, params_hash)
